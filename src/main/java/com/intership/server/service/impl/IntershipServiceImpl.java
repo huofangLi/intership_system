0c0e7b65ade369a7fed0de8f86895e57f93487eb
王德旺 <1834257441@qq.com>
2020-06-01 17:01:07 +0800

    20200601，接口修改

--- src/main/java/com/intership/server/service/impl/IntershipServiceImpl.java
+++ src/main/java/com/intership/server/service/impl/IntershipServiceImpl.java
@@ -3,14 +3,18 @@ package com.intership.server.service.impl;
 import com.intership.server.service.IntershipService;
 import com.intership.server.domain.Intership;
 import com.intership.server.repository.IntershipRepository;
+import liquibase.executor.LoggingExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -23,9 +27,11 @@ public class IntershipServiceImpl implements IntershipService {
     private final Logger log = LoggerFactory.getLogger(IntershipServiceImpl.class);
 
     private final IntershipRepository intershipRepository;
+    private final JdbcTemplate jdbcTemplate;
 
-    public IntershipServiceImpl(IntershipRepository intershipRepository) {
+    public IntershipServiceImpl(IntershipRepository intershipRepository, JdbcTemplate jdbcTemplate) {
         this.intershipRepository = intershipRepository;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     /**
@@ -76,5 +82,27 @@ public class IntershipServiceImpl implements IntershipService {
     public void delete(Long id) {
         log.debug("Request to delete Intership : {}", id);
         intershipRepository.deleteById(id);
+    }
+
+    /**
+     * 根据学生 ID 查询实习
+     * @param stuId
+     * @return
+     */
+    @Override
+    public List<Intership> findByStuId(Long stuId) {
+        return intershipRepository.findByStuIdId(stuId);
+    }
+
+    /**
+     * 根据学生 ID 查询实习成绩
+     * @param stuId
+     * @return
+     */
+    @Override
+    public List<Map<String, Object>> findGradeByStuId(Long stuId) {
+        String sql = "SELECT tutor_score,master_score,intership_score FROM intership WHERE stu_id_id =" + stuId;
+        List<Map<String, Object>> Grade = jdbcTemplate.queryForList(sql);
+        return Grade;
     }
 }
